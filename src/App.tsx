47e88defffa9e8579fb2c64627b8eb7d78efcece
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-01-22 18:42:16 +0100

    'added Item component to display items'

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 // Components
+import Item from './Item/Item';
 import Drawer from '@material-ui/core/Drawer';
-import LinerarProgress from '@material-ui/core/LinearProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
@@ -31,9 +32,26 @@ const App = () => {
   );
   console.log(data);
 
+  const getTotalItems = () => null;
 
+  const handleAddToCart = (clickedItem: CartItemType) => null;
 
-  return <div className="App">Hello!</div>;
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;

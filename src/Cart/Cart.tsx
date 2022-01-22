bac7c282b784f9b3c9e52bad6235f27a790b7ee3
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-01-22 20:27:43 +0100

    Added calculation of total to cart

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -12,6 +12,9 @@ type Props = {
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
 
+    const calculateTotal = (items: CartItemType[]) =>
+        items.reduce((acc: number, item) => acc + item.amount * item.price, 0);
+
     return (
         <Wrapper>
             <h2>Your Shopping Cart</h2>
@@ -24,6 +27,7 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
                     removeFromCart={removeFromCart}
                 />
             ))}
+            <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
         </Wrapper>
     )
 }

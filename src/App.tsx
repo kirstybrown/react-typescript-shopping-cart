6ab00c36cdf01486777d38dcf2eb36ba94a99d3c
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-01-22 18:07:02 +0100

    first commit with basic config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from "react";
+import { useQuery } from "react-query";
+// Components
+import Drawer from '@material-ui/core/Drawer';
+import LinerarProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+//Styles
+import { Wrapper } from "./App.styles";
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
 }
+
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products', 
+    getProducts
+  );
+  console.log(data);
+
+
+
+  return <div className="App">Hello!</div>;
+};
 
 export default App;
